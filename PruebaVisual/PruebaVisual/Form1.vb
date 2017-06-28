4e1b459cbad6e9d3ed4b103e449d3f471decb69c
JgonzalezCSE <JgonzalezCSE@grupocse.cr>
2017-06-28 12:11:46 -0600

    Boton que cuenta de 1 a 5

---
+++ PruebaVisual/PruebaVisual/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As System.Object, e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        For index As Integer = 1 To 5
+            Label1.Text = index
+            Me.Refresh()
+            System.Threading.Thread.Sleep(1000)
+        Next
+
+
+
+
+
+        'Me.Refresh()
+        'System.Threading.Thread.Sleep(1000)
+        'Label1.Text = "2"
+        'Me.Refresh()
+        'System.Threading.Thread.Sleep(1000)
+        'Label1.Text = "3"
+        'Me.Refresh()
+        'System.Threading.Thread.Sleep(1000)
+        'Label1.Text = "4"
+        'Me.Refresh()
+        'System.Threading.Thread.Sleep(1000)
+        'Label1.Text = "5"
+
+    End Sub
+End Class
